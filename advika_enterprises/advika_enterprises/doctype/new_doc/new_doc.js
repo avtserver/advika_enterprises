4a9ee92b3e4103e73d006557ae0396565ef95c5a
Faiyaz <sacademy@avtutoring.in>
2023-07-11 02:10:41 +0530

    update

--- advika_enterprises/advika_enterprises/doctype/new_doc/new_doc.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, MD Faiyaz Ansari and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("new doc", {
-// 	refresh(frm) {
-
-// 	},
-// });
